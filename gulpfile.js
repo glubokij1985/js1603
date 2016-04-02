31127812a51372425e86699b493d6811164b802e
glubokij1985 <glubokij85@gmail.com>
2016-04-02 23:34:07 +0300

    4s commit

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,15 @@ var gulp = require('gulp'),
 	cssmin = require('gulp-cssmin'),
 	rename = require('gulp-rename');
 
+gulp.task('concat', function () {
+  return gulp.src('public/css/*.css')
+    .pipe(concatCss("bundle.css"))
+    .pipe(gulp.dest('public/build/'));
+});
 gulp.task('css', function () {
-  return gulp.src('./public/css/*.css')
+  return gulp.src('public/css/*.css')
     .pipe(concatCss("bundle.css"))
     .pipe(cssmin())
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./public/build/'));
+    .pipe(gulp.dest('public/build/'));
 });
